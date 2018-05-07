1e34c1fe3297942c07176df8809b28b022fdb1c7
Raul2762 <raulrodriguez1992@icloud.com>
2018-05-07 14:15:20 -0400

    Agregado de iconos, diccionario de recurso de iconos y codigo para cambiarlos

--- MessageBoxCustomRM/MessageWindows.xaml.cs
+++ MessageBoxCustomRM/MessageWindows.xaml.cs
@@ -34,6 +34,16 @@ namespace MessageBoxCustomRM
             this.Left = (screenWidth / 2) - (windowWidth / 2);
             this.Top = (screenHeight / 2) - (windowHeight / 2);
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ImgShow.Source = (ImageSource)FindResource("Question");
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ImgShow.Source = (ImageSource)FindResource("Error");
+        }
     }
 
 
