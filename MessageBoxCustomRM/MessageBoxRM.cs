5d2f3b1af42d9e260e41e7b30da08df5ae661fb3
Raul2762 <raulrodriguez1992@icloud.com>
2018-05-09 04:41:52 -0400

    Etapa Pre-finalizada se hicieron cambios de retorno de respuesta visuales y de redimencionado

--- MessageBoxCustomRM/MessageBoxRM.cs
+++ MessageBoxCustomRM/MessageBoxRM.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
 
 namespace MessageBoxCustomRM
 {
@@ -13,9 +16,25 @@ namespace MessageBoxCustomRM
         OKCancel,
         YesNo
     }
+    public enum MessageBoxIconRM
+    {
+        Information,
+        Question,
+        Warning,
+        Error
+    }
     public static class MessageBoxRM
     {
-       
+        //
+        // Resumen:
+        //     Muestra un cuadro de mensaje con el texto especificado.
+        //
+        // Parámetros:
+        //   text:
+        //     Texto que se va a mostrar en el cuadro de mensaje.
+        //
+        // Devuelve:
+        //     Uno de los valores de System.Windows.MessageBoxResult.
         public static MessageBoxResult Show(String text)
         {
             MessageBoxResult result = MessageBoxResult.No;
@@ -23,8 +42,9 @@ namespace MessageBoxCustomRM
             abrir.btn1.Visibility = Visibility.Hidden;
             abrir.btn2.Content = "Aceptar";
             abrir.Contenido = text;
-            abrir.Show();
-
+            abrir.Title = "";
+            abrir.ShowDialog();
+            result = abrir.ResultRetornado;
             return result;
         }
 
@@ -36,8 +56,8 @@ namespace MessageBoxCustomRM
             abrir.btn2.Content = "Aceptar";
             abrir.Title = title;
             abrir.Contenido = text;
-            abrir.Show();
-
+            abrir.ShowDialog();
+            result = abrir.ResultRetornado;
             return result;
         }
 
@@ -60,12 +80,63 @@ namespace MessageBoxCustomRM
                     abrir.btn2.Content = "No";
                     break;
                 default:
+                    abrir.btn1.Visibility = Visibility.Hidden;
+                    abrir.btn2.Content = "Aceptar";
                     break;
             }
             abrir.Title = title;
             abrir.Contenido = text;
-            abrir.Show();
+            abrir.ShowDialog();
+            result = abrir.ResultRetornado;
+            return result;
+        }
 
+        public static MessageBoxResult Show(String text, String title, MessageBoxButtonRM buttons,MessageBoxIconRM icon)
+        {
+            MessageBoxResult result = MessageBoxResult.No;
+            MessageWindows abrir = new MessageWindows();
+            switch (buttons)
+            {
+                case MessageBoxButtonRM.OK:
+                    abrir.btn1.Visibility = Visibility.Hidden;
+                    abrir.btn2.Content = "Aceptar";
+                    break;
+                case MessageBoxButtonRM.OKCancel:
+                    abrir.btn1.Content = "Aceptar";
+                    abrir.btn2.Content = "Cancelar";
+                    break;
+                case MessageBoxButtonRM.YesNo:
+                    abrir.btn1.Content = "Si";
+                    abrir.btn2.Content = "No";
+                    break;
+                default:
+                    abrir.btn1.Visibility = Visibility.Hidden;
+                    abrir.btn2.Content = "Aceptar";
+                    break;
+            }
+
+            switch (icon)
+            {
+                case MessageBoxIconRM.Information:
+                    abrir.ImgShow.Source = new BitmapImage(new Uri("pack://application:,,,/MessageBoxCustomRM;component/information.png"));
+                    break;
+                case MessageBoxIconRM.Question:
+                    abrir.ImgShow.Source = new BitmapImage(new Uri("pack://application:,,,/MessageBoxCustomRM;component/help-circle.png"));
+                    break;
+                case MessageBoxIconRM.Warning:
+                    abrir.ImgShow.Source = new BitmapImage(new Uri("pack://application:,,,/MessageBoxCustomRM;component/alert.png"));
+                    break;
+                case MessageBoxIconRM.Error:
+                    abrir.ImgShow.Source = new BitmapImage(new Uri("pack://application:,,,/MessageBoxCustomRM;component/icon-error.png"));
+                    break;
+                default:
+                    abrir.ImgShow.Source = new BitmapImage(new Uri("pack://application:,,,/MessageBoxCustomRM;component/information.png"));
+                    break;
+            }
+            abrir.Title = title;
+            abrir.Contenido = text;
+            abrir.ShowDialog();
+            result = abrir.ResultRetornado;
             return result;
         }
     }
--- MessageBoxCustomRM/MessageWindows.xaml.cs
+++ MessageBoxCustomRM/MessageWindows.xaml.cs
@@ -21,6 +21,7 @@ namespace MessageBoxCustomRM
     public partial class MessageWindows : MetroWindow
     {
         public string Contenido { get; set; }
+        public MessageBoxResult ResultRetornado { get; set; }
         public MessageWindows()
         {
             InitializeComponent();
@@ -35,11 +36,44 @@ namespace MessageBoxCustomRM
             this.Left = (screenWidth / 2) - (windowWidth / 2);
             this.Top = (screenHeight / 2) - (windowHeight / 2);
         }
-        
+
+        public void Redimensionar()
+        {
+            double alto = lblContent.RenderSize.Height;
+            if (alto > 82)
+            {
+                this.Height = alto + 115;
+                CenterWindowOnScreen();
+            }
+            else
+            {
+                this.Height = 206;
+                CenterWindowOnScreen();
+            }
+            
+        }
+
         private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
         {
             lblContent.Text = Contenido;
             //ImgShow.Source = (ImageSource)FindResource("Question");
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            ResultRetornado = MessageBoxResult.OK;
+            this.Close();
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            ResultRetornado = MessageBoxResult.Cancel;
+            this.Close();
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+                Redimensionar();
         }
     }
 
